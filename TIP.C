4e02230272fcf7e3760951edd55b5281a83fb21c
happydigit <45298210+happydigit@users.noreply.github.com>
2021-01-23 11:52:49 +0100

    Update TIP.C
    Fit Boltzmann
    Loi de puissance
    Legende

--- TIP.C
+++ TIP.C
@@ -8,12 +8,22 @@
 #include <math.h> 
 #include <TRandom.h>
 
-
-double fitf(double x, double par1, double par2) {
-      double kb = 1.380649*pow(10,-23);
-      double res = par1*exp(-x / kb*par2 ) ;
+		// Definition de la fonction de Boltzmann
+		
+double fitf(double *x, double *par1) {
+      double res = sqrt(2/3.14)*((x[0]+par1[1])*(x[0]+par1[1])*exp(-(x[0]+par1[1])*(x[0]+par1[1])/(2*par1[0]*par1[0]))/par1[0]*par1[0]*par1[0]) ;
       return res;
    }
+   
+double plaw(double *x, double*par) {
+	double res = par[0]*pow(x[0],par[1]);
+	return res ;
+}
+
+double bol(double *x, double*par) {
+	double res = par[0]*exp(-x[0]/par[1]);
+	return res ;
+}
 
 int main(){
 cout << " début " << endl ;
@@ -130,28 +140,36 @@ for(int i = 0; i <= Nbinx  ; i++){
 	
 }
 
-/*
-		// Fit par rapport à la distribution de boltzmann
-TF1 *func = new TF1("boltzmann",fitf,-3,3,3);
-func->SetParameters(500,H1_pp->GetMean(),H1_pp->GetRMS());
 
-*/
+		//Scaling en prépartion du fit
 
-		// Création des canvas
-TCanvas* T1 = new TCanvas("Canvas 1" , " CANVAS_1_PP1 " , 200 , 200 );
-TCanvas* T2 = new TCanvas("Canvas 2" , " CANVAS_2_PP1 " , 200 , 200 );
-TCanvas* T3 = new TCanvas("Canvas 3" , " CANVAS_3_PP1 " , 200 , 200 );
-TCanvas* T4 = new TCanvas("Canvas 4" , " CANVAS_4_PP1 " , 200 , 200 );
-TCanvas* T5 = new TCanvas("Canvas 5" , " CANVAS_5_PP1 " , 200 , 200 );
-TCanvas* T6 = new TCanvas("Canvas 6" , " CANVAS_6_PP1 " , 200 , 200 );
-TCanvas* T7 = new TCanvas("Canvas 7" , " CANVAS_7_PP1 " , 200 , 200 );
-TCanvas* T8 = new TCanvas("Canvas 8" , " CANVAS_8_PP1 " , 200 , 200 );
-TCanvas* T9 = new TCanvas("Canvas 9" , " CANVAS_9_PP1 " , 200 , 200 );
-TCanvas* T10 = new TCanvas("Canvas 10" , " CANVAS_10_PP1 " , 200 , 200 );
+double Scale_Histo[11] ;
+Scale_Histo[1]=H1_pp->Integral() ;
+Scale_Histo[2]=H2_pp->Integral() ;
+Scale_Histo[3]=H3_pp->Integral() ;
+Scale_Histo[4]=H4_pp->Integral() ;
+Scale_Histo[5]=H5_pp->Integral() ;
+Scale_Histo[6]=H6_pp->Integral() ;
+Scale_Histo[7]=H7_pp->Integral() ;
+Scale_Histo[8]=H8_pp->Integral() ;
+Scale_Histo[9]=H9_pp->Integral() ;
+Scale_Histo[10]=H10_pp->Integral() ;
+
+
+H1_pp->Scale(1/H1_pp->Integral());
+H2_pp->Scale(1/H2_pp->Integral());
+H3_pp->Scale(1/H3_pp->Integral());
+H4_pp->Scale(1/H4_pp->Integral());
+H5_pp->Scale(1/H5_pp->Integral());
+H6_pp->Scale(1/H6_pp->Integral());
+H7_pp->Scale(1/H7_pp->Integral());
+H8_pp->Scale(1/H8_pp->Integral());
+H9_pp->Scale(1/H9_pp->Integral());
+H10_pp->Scale(1/H10_pp->Integral());
 
 
 		// Changement de range sur l'axe X
-
+		
 H1_pp->SetAxisRange(0,5,"X");
 H2_pp->SetAxisRange(0,5,"X");
 H3_pp->SetAxisRange(0,5,"X");
@@ -164,71 +182,173 @@ H9_pp->SetAxisRange(0,5,"X");
 H10_pp->SetAxisRange(0,5,"X");
 
 
+		// Fit par rapport à la distribution de boltzmann
+		
+TF1 *func = new TF1("boltzmann",fitf,0,20,2);
+func->SetParameter(0,0.1);
+func->SetParameter(1,0.2);
+func->SetParNames("a","shift");
+func->SetLineColor(kViolet);
+
+TF1 *func2 = new TF1("Plaw",plaw,0,20,2);
+func2->SetParameters(0,-1);
+func2->SetParNames("a","k");	// y = a*x^k
+func2->SetLineColor(kGreen);
+
+TF1 *func3 = new TF1("bol",bol,0,20,2);
+func3->SetParameter(0,0.05);
+func3->SetParameter(1,0.01);
+func3->SetParNames("N","Kb*T");	// y = a*x^k
+func3->SetLineColor(kCyan);
+
 		// Sauvegarde des canvas dans l'output-histo-file càd notre result.root
 
+		// Création des canvas
+TCanvas* T1 = new TCanvas("Canvas 1" , " CANVAS_1_PP1 " , 200 , 200 );
+TCanvas* T2 = new TCanvas("Canvas 2" , " CANVAS_2_PP1 " , 200 , 200 );
+TCanvas* T3 = new TCanvas("Canvas 3" , " CANVAS_3_PP1 " , 200 , 200 );
+TCanvas* T4 = new TCanvas("Canvas 4" , " CANVAS_4_PP1 " , 200 , 200 );
+TCanvas* T5 = new TCanvas("Canvas 5" , " CANVAS_5_PP1 " , 200 , 200 );
+TCanvas* T6 = new TCanvas("Canvas 6" , " CANVAS_6_PP1 " , 200 , 200 );
+TCanvas* T7 = new TCanvas("Canvas 7" , " CANVAS_7_PP1 " , 200 , 200 );
+TCanvas* T8 = new TCanvas("Canvas 8" , " CANVAS_8_PP1 " , 200 , 200 );
+TCanvas* T9 = new TCanvas("Canvas 9" , " CANVAS_9_PP1 " , 200 , 200 );
+TCanvas* T10 = new TCanvas("Canvas 10" , " CANVAS_10_PP1 " , 200 , 200 );
+		// Sauvegarde des canvas dans l'output-histo-file càd notre result.root
+		// Appels des différents fits
+gStyle->SetOptFit(0);
+gStyle->SetOptStat(1111);
+	
 OutputHisto->cd();
-
 
 T1->cd();
 H1_pp->Draw("][ E1");
-H1_pp->Fit("expo", "L" ,"",0.1 ,5);
-H1_pp->Fit("boltzmann", "L" ,"SAME HIST",0.1 ,5);
+H1_pp->Fit("bol", "L" ,"SAME HIST",0 ,20);
+H1_pp->Fit("boltzmann", "L + I M R B" ,"SAME HIST",0 ,20);
+H1_pp->Fit("Plaw","L + I R B","SAME HIST",0,20);
+H1_pp->GetFunction("Plaw")->SetLineColor(kGreen);
+H1_pp->GetFunction("boltzmann")->SetLineColor(kViolet);
+H1_pp->GetFunction("bol")->SetLineColor(kCyan);
+		// Configuration de la légende
+auto legend = new TLegend(0.4,0.7,0.6,0.9);
+//legend->SetHeader("The Legend Title","C"); // option "C" allows to center the header
+legend->AddEntry(H1_pp,"Data","lep");
+legend->AddEntry(func,"Maxwell-Boltzmann fit","l");
+legend->AddEntry(func2,"Power law fit","l");
+legend->AddEntry(func3,"Boltzmann fit","l");
+legend->Draw();
 
 T1->Write();
 
+
 T2->cd();
 H2_pp->Draw("][ E1");
-H2_pp->Fit("expo", "L" ,"SAME HIST",0.1 ,5);
-//H2_pp->Fit("boltzmann", "L" ,"SAME HIST",0.1 ,5);
+H2_pp->Fit("expo", "L" ,"SAME HIST",0 ,20);
+H2_pp->Fit("boltzmann", "L + I M R B" ,"SAME HIST",0 ,20);
+H2_pp->Fit("Plaw","L + I R B","SAME HIST",0,20);
+H2_pp->GetFunction("Plaw")->SetLineColor(kGreen);
+H2_pp->GetFunction("boltzmann")->SetLineColor(kViolet);
+H2_pp->GetFunction("expo")->SetLineColor(kCyan);
+legend->Draw();
+
 T2->Write();
 
 T3->cd();
 H3_pp->Draw("][ E1");
-H3_pp->Fit("expo", "L" ,"SAME HIST",0.1 ,5);
-H3_pp->Fit("boltzmann", "L" ,"SAME HIST",0.1 ,5);
-T3->Write();
+H3_pp->Fit("expo", "L" ,"SAME HIST",0 ,20);
+H3_pp->Fit("boltzmann", "L + I M R B" ,"SAME HIST",0 ,20);
+H3_pp->Fit("Plaw","L + I R B","SAME HIST",0,20);
+H3_pp->GetFunction("Plaw")->SetLineColor(kGreen);
+H3_pp->GetFunction("boltzmann")->SetLineColor(kViolet);
+H3_pp->GetFunction("expo")->SetLineColor(kCyan);
+legend->Draw();
 
+T3->Write();
+ 
 T4->cd();
 H4_pp->Draw("][ E1");
-H4_pp->Fit("expo", "L" ,"SAME HIST",0.1 ,5);
-H4_pp->Fit("boltzmann", "L" ,"SAME HIST",0.1 ,5);
+H4_pp->Fit("expo", "L" ,"SAME HIST",0 ,20);
+H4_pp->Fit("boltzmann", "L + I M R B" ,"SAME HIST",0 ,20);
+H4_pp->Fit("Plaw","L + I R B","SAME HIST",0,20);
+H4_pp->GetFunction("Plaw")->SetLineColor(kGreen);
+H4_pp->GetFunction("boltzmann")->SetLineColor(kViolet);
+H4_pp->GetFunction("expo")->SetLineColor(kCyan);
+legend->Draw();
+
 T4->Write();
 
 T5->cd();
 H5_pp->Draw("][ E1");
-H5_pp->Fit("expo", "L" ,"SAME HIST",0.1 ,5);
-H5_pp->Fit("boltzmann", "L" ,"SAME HIST",0.1 ,5);
+H5_pp->Fit("expo", "L" ,"SAME HIST",0 ,20);
+H5_pp->Fit("boltzmann", "L + I M R B" ,"SAME HIST",0 ,20);
+H5_pp->Fit("Plaw","L + I R B","SAME HIST",0,20);
+H5_pp->GetFunction("Plaw")->SetLineColor(kGreen);
+H5_pp->GetFunction("boltzmann")->SetLineColor(kViolet);
+H5_pp->GetFunction("expo")->SetLineColor(kCyan);
+legend->Draw();
+
 T5->Write();
 
 T6->cd();
 H6_pp->Draw("][ E1");
-H6_pp->Fit("expo", "L" ,"SAME HIST",0.1 ,5);
-H6_pp->Fit("boltzmann", "L" ,"SAME HIST",0.1 ,5);
+H6_pp->Fit("expo", "L" ,"SAME HIST",0 ,20);
+H6_pp->Fit("boltzmann", "L + I M R B" ,"SAME HIST",0 ,20);
+H6_pp->Fit("Plaw","L + I R B","SAME HIST",0,20);
+H6_pp->GetFunction("Plaw")->SetLineColor(kGreen);
+H6_pp->GetFunction("boltzmann")->SetLineColor(kViolet);
+H6_pp->GetFunction("expo")->SetLineColor(kCyan);
+legend->Draw();
+
 T6->Write();
 
 T7->cd();
 H7_pp->Draw("][ E1");
-H7_pp->Fit("expo", "L" ,"SAME HIST",0.1 ,5);
-H7_pp->Fit("boltzmann", "L" ,"SAME HIST",0.1 ,5);
+H7_pp->Fit("expo", "L" ,"SAME HIST",0 ,20);
+H7_pp->Fit("boltzmann", "L + I M R B" ,"SAME HIST",0 ,20);
+H7_pp->Fit("Plaw","L + I R B","SAME HIST",0,20);
+H7_pp->GetFunction("Plaw")->SetLineColor(kGreen);
+H7_pp->GetFunction("boltzmann")->SetLineColor(kViolet);
+H7_pp->GetFunction("expo")->SetLineColor(kCyan);
+legend->Draw();
+
 T7->Write();
 
 T8->cd();
 H8_pp->Draw("][ E1");
-H8_pp->Fit("expo", "L" ,"SAME HIST",0.1 ,5);
-H8_pp->Fit("boltzmann", "L" ,"SAME HIST",0.1 ,5);
+H8_pp->Fit("expo", "L" ,"SAME HIST",0 ,20);
+H8_pp->Fit("boltzmann", "L + I M R B" ,"SAME HIST",0 ,20);
+H8_pp->Fit("Plaw","L + I R B","SAME HIST", 0 , 20);
+H8_pp->GetFunction("Plaw")->SetLineColor(kGreen);
+H8_pp->GetFunction("boltzmann")->SetLineColor(kViolet);
+H8_pp->GetFunction("expo")->SetLineColor(kCyan);
+legend->Draw();
+
 T8->Write();
 
 T9->cd();
 H9_pp->Draw("][ E1");
-H9_pp->Fit("expo", "L" ,"SAME HIST",0.1 ,5);
-H9_pp->Fit("boltzmann", "L" ,"SAME HIST",0.1 ,5);
+H9_pp->Fit("expo", "L" ,"SAME HIST",0 ,20);
+H9_pp->Fit("boltzmann", "L + I M R B" ,"SAME HIST",0 ,20);
+H9_pp->Fit("Plaw","L + I R B","SAME HIST",0,20);
+H9_pp->GetFunction("Plaw")->SetLineColor(kGreen);
+H9_pp->GetFunction("boltzmann")->SetLineColor(kViolet);
+H9_pp->GetFunction("expo")->SetLineColor(kCyan);
+legend->Draw();
+
 T9->Write();
 
 T10->cd();
 H10_pp->Draw("][ E1");
-H10_pp->Fit("expo", "L" ,"SAME HIST",0.1 ,5);
-H10_pp->Fit("boltzmann", "L" ,"SAME HIST",0.1 ,5);
+H10_pp->Fit("expo", "L" ,"SAME HIST",0 ,20);
+H10_pp->Fit("boltzmann", "L + I M R B" ,"SAME HIST",0 ,20);
+H10_pp->Fit("Plaw","L + I R B","SAME HIST",0,20);
+H10_pp->GetFunction("Plaw")->SetLineColor(kGreen);
+H10_pp->GetFunction("boltzmann")->SetLineColor(kViolet);
+H10_pp->GetFunction("expo")->SetLineColor(kCyan);
+legend->Draw();
+
 T10->Write();
+
 
 OutputHisto->Close();
 
